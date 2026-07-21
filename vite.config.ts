e64781dd42e93fe829f68ae07296c6179917a266
miular <18268869810@163.com>
2026-07-21 11:17:16 +0800

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/miular/celestial-motion-simulation/",
+  base: "/celestial-motion-simulation/",
   build: {
     rollupOptions: {
       output: {
